28a8be2190e3e7598f4bc11b8dba3c142e8e02cb
Zhen NI <z.ni@hotmail.com>
2026-09-12 22:34:17 +0800

    Add indicator_daily

--- eatmud_core/src/strategy/indicator.rs
+++ eatmud_core/src/strategy/indicator.rs
@@ -3,6 +3,36 @@ use crate::TransactionIterator;
 use crate::Weekday;
 
 /// Buy if indicator is larger than 0.
+pub fn indicator_daily(
+    it: &mut TransactionIterator,
+    indicators: &[&HistoryView<f64>],
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut status = vec![false; it.nfunds()];
+    let position = 1.0 / it.nfunds() as f64;
+    while it.next_day().is_some() {
+        let index = it.index();
+        if index < 1 {
+            continue;
+        }
+        for i in 0..it.nfunds() {
+            let s = &mut status[i];
+            let indicator = indicators[i].get(it).unwrap();
+            let v = indicator[index - 1];
+            if v > 0.0 && !*s {
+                it.position(i, position, 0.0, false)?;
+                *s = true;
+            }
+            if v < 0.0 && *s {
+                it.position(i, 0.0, 0.0, false)?;
+                *s = false;
+            }
+        }
+    }
+    Ok(())
+}
+
+
+/// Buy if indicator is larger than 0.
 pub fn indicator_weekly(
     it: &mut TransactionIterator,
     weekday: Weekday,
@@ -45,13 +75,13 @@ mod test {
         }
         r.push(0.);
         for i in 1..v.len() {
-            r.push(v[i] - v[i-1]);
+            r.push(v[i] - v[i - 1]);
         }
         r
     }
-    
+
     #[test]
-    fn test_extremum_ma() {
+    fn test_extremum_ma_weekly() {
         let mut hs300 = Fund::from(&read_tdx("../tdx/test-hs300.txt").unwrap());
         let mut gz2000 = Fund::from(&read_tdx("../tdx/test-gz2000.txt").unwrap());
         let start_date = NaiveDate::parse_from_str("20110101", "%Y%m%d").ok();
@@ -74,6 +104,33 @@ mod test {
                 resi.push(it.asset());
             }
             results.push(resi);
+        }
+
+        for i in results {
+            println!("{:?}", i);
+        }
+    }
+
+    #[test]
+    fn test_extremum_ma_daily() {
+        let mut hs300 = Fund::from(&read_tdx("../tdx/test-hs300.txt").unwrap());
+        let mut gz2000 = Fund::from(&read_tdx("../tdx/test-gz2000.txt").unwrap());
+        let start_date = NaiveDate::parse_from_str("20110101", "%Y%m%d").ok();
+        let end_date = NaiveDate::parse_from_str("20240101", "%Y%m%d").ok();
+        let trans = Transaction::new(&[&hs300, &gz2000], start_date, end_date);
+
+        hs300.truncate(start_date, end_date);
+        gz2000.truncate(start_date, end_date);
+
+        let mut results = Vec::new();
+        for ma_period in [5, 10, 20, 30, 60, 120, 200, 500] {
+            let ind1 = HistoryView::from_vec(&trans, diff(hs300.ma(ma_period))).unwrap();
+            let ind2 = HistoryView::from_vec(&trans, diff(gz2000.ma(ma_period))).unwrap();
+            let inds = [&ind1, &ind2];
+            let mut it = trans.iter(false, true);
+            it.inflow(1.0).unwrap();
+            indicator_daily(&mut it, &inds).unwrap();
+            results.push(it.asset());
         }
 
         for i in results {
--- eatmud_core/src/strategy/mod.rs
+++ eatmud_core/src/strategy/mod.rs
@@ -3,5 +3,5 @@ pub mod indicator;
 pub mod kelly;
 
 pub use aip::aip_monthly;
+pub use indicator::{indicator_daily, indicator_weekly};
 pub use kelly::{kelly_hint, kelly_weekly};
-pub use indicator::indicator_weekly;
